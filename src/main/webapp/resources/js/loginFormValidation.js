cc593ca938fd290408a3fcb93d78dc945eb96682
Dzmitry Babanin <babanin.dima@gmail.com>
2019-11-13 21:17:44 +0300

    fix: removed the function for checking the username when you click on the form button

--- src/main/webapp/resources/js/loginFormValidation.js
+++
@@ -1,9 +0,0 @@
-function checkIsNotEmptyUsername(form, message) {
-    let inputUsername = form[form.id + ":username"];
-    if (inputUsername.value === '') {
-        alert(message);
-        inputUsername.focus();
-        return false;
-    }
-    return true;
-}
